4ba013d3b284fa3b36a201e7da04766a61078b02
Ильдар <162967672+Kweit1989@users.noreply.github.com>
2025-04-14 17:04:56 +0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const headerImg = document.getElementById('header-img');
-  const menuItems = document.querySelectorAll('.menu-item');
-
-  // Функция для корректировки высоты в зависимости от устройства
-  function setFullHeight() {
-      const headerHeight = 69; // Высота шапки
-      const viewportHeight = window.innerHeight; // Полная высота экрана устройства
-      const adjustedHeight = viewportHeight - headerHeight; // Вырезаем высоту шапки
-      document.querySelectorAll('.header, .header-img').forEach(element => {
-          element.style.height = `${adjustedHeight}px`; // Устанавливаем высоту для этих элементов
-      });
-  }
-
-  // Запуск функции при загрузке страницы и изменении размера окна
-  setFullHeight();
-  window.addEventListener('resize', setFullHeight); // Для корректной работы на мобильных устройствах
-
-  let isFaded = true;
-
-  setInterval(() => {
+    const headerImg = document.getElementById('header-img');
+    const menuItems = document.querySelectorAll('.menu-item');
+  
+    let isFaded = true;
+  
+    setInterval(() => {
       // Меняем прозрачность у header-img
       headerImg.style.opacity = isFaded ? '0.5' : '0.1';
-
+  
       // Переключаем класс у всех меню-элементов
       menuItems.forEach(item => {
-          item.classList.toggle('active', isFaded);
+        item.classList.toggle('active', isFaded);
       });
-
+  
       isFaded = !isFaded;
-  }, 3000);
-});
+    }, 3000);
+  });
+  
