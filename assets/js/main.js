537daf3107d639ef181e989b479bee86f7bc0f4b
Ильдар <162967672+Kweit1989@users.noreply.github.com>
2025-04-14 18:33:04 +0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const headerImg = document.getElementById('header-img');
-    const menuItems = document.querySelectorAll('.menu-item');
+  const headerImg = document.getElementById('header-img');
+  const menuItems = document.querySelectorAll('.menu-item');
 
-    // Функция для корректировки высоты в зависимости от устройства
-    function setFullHeight() {
-        const headerHeight = 69; // Высота шапки
-        const viewportHeight = window.innerHeight; // Полная высота экрана устройства
-        const adjustedHeight = viewportHeight - headerHeight; // Вырезаем высоту шапки
-        document.querySelectorAll('.header, .header-img').forEach(element => {
-            element.style.height = `${adjustedHeight}px`; // Устанавливаем высоту для этих элементов
-        });
-    }
+  function setFullHeight() {
+      const headerHeight = 69; 
+      const viewportHeight = window.innerHeight;
+      const adjustedHeight = viewportHeight - headerHeight;
 
-    // Запуск функции при загрузке страницы и изменении размера окна
-    setFullHeight();
-    window.addEventListener('resize', setFullHeight); // Для корректной работы на мобильных устройствах
+      document.querySelectorAll('.header, .header-img').forEach(element => {
+          element.style.height = `${adjustedHeight}px`; 
+      });
+  }
 
-    let isFaded = true;
+  // Инициализация высоты при загрузке
+  setFullHeight();
 
-    setInterval(() => {
-        // Меняем прозрачность у header-img
-        headerImg.style.opacity = isFaded ? '0.5' : '0.1';
+  // Используем requestAnimationFrame для предотвращения "прыжков"
+  window.addEventListener('resize', () => {
+      window.requestAnimationFrame(setFullHeight);
+  });
 
-        // Переключаем класс у всех меню-элементов
-        menuItems.forEach(item => {
-            item.classList.toggle('active', isFaded);
-        });
+  let isFaded = true;
 
-        isFaded = !isFaded;
-    }, 3000);
+  setInterval(() => {
+      headerImg.style.opacity = isFaded ? '0.5' : '0.1';
+
+      menuItems.forEach(item => {
+          item.classList.toggle('active', isFaded);
+      });
+
+      isFaded = !isFaded;
+  }, 3000);
 });
