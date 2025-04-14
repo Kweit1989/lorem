df25a33a238fab065438818ac9352ac361191c07
Ильдар <162967672+Kweit1989@users.noreply.github.com>
2025-04-14 15:10:19 +0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const headerImg = document.getElementById('header-img');
+    const menuItems = document.querySelectorAll('.menu-item');
+  
     let isFaded = true;
   
     setInterval(() => {
+      // Меняем прозрачность у header-img
       headerImg.style.opacity = isFaded ? '0.5' : '0.1';
+  
+      // Переключаем класс у всех меню-элементов
+      menuItems.forEach(item => {
+        item.classList.toggle('active', isFaded);
+      });
+  
       isFaded = !isFaded;
-    }, 3000); // каждые 3 секунды
+    }, 3000);
   });
+  
